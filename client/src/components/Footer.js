4dba6efb5b0f4e1fa08ae4eddaca668f6b687c98
AlexanderDLe <iamxanofficial@gmail.com>
2018-07-01 18:46:31 -0700

    Service CSS change.

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -8,19 +8,44 @@ import ytIcon from '../img/YT Icon.webp';
 export default () => {
   return (
     <footer className="text-center text-light">
-      <a className="icon" href="">
+      <a
+        className="icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.facebook.com/VvnZQuan/"
+      >
         <img src={fbIcon} alt="" />
       </a>
-      <a className="icon" href="">
+      <a
+        className="icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.instagram.com/vvnzquan/"
+      >
         <img src={igIcon} alt="" />
       </a>
-      <a className="icon" href="">
+      <a
+        className="icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://soundcloud.com/vvnzquan"
+      >
         <img src={scIcon} alt="" />
       </a>
-      <a className="icon" href="">
+      <a
+        className="icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/VvnZQuan"
+      >
         <img src={tIcon} alt="" />
       </a>
-      <a className="icon" href="">
+      <a
+        className="icon"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.youtube.com/xanandvvn"
+      >
         <img src={ytIcon} alt="" />
       </a>
       <p className="cinzel">&copy; VVN </p>
