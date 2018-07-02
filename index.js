25a8fcec3968688913a331e695397d7fae3f76a0
AlexanderDLe <iamxanofficial@gmail.com>
2018-07-01 22:24:47 -0700

    Prepared for deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 const key = require('./config/key');
+const path = require('path');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -61,3 +62,14 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
+
+// Serve static assets if in production
+
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
