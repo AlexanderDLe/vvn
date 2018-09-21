dba6aa087c152a4f942f5e38942c618758600102
AlexanderDLe <iamxanofficial@gmail.com>
2018-09-21 16:42:13 -0700

    VVN Site Copied from Tester

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
-import { HashRouter, Route } from 'react-router-dom';
 import VVNLogo from './img/VVN Logo.png';
 
 import Navigation from './components/Navigation';
@@ -15,21 +15,21 @@ import Contact from './components/Contact';
 class App extends Component {
   render() {
     return (
-      <HashRouter>
-        <div className="App">
-          <Navigation />
-          <div className="background">
-            <img id="VVN-BG" src={VVNLogo} alt="" />
+      <div className="App">
+        <Navigation />
+        <div className="background">
+          <img id="VVN-BG" src={VVNLogo} alt="" />
+          <Switch>
             <Route exact path="/" component={Landing} />
             <Route exact path="/about" component={About} />
             <Route exact path="/music" component={Music} />
             <Route exact path="/socials-streams" component={Socials_Streams} />
             <Route exact path="/services" component={Services} />
             <Route exact path="/contact" component={Contact} />
-          </div>
-          <Footer />
+          </Switch>
         </div>
-      </HashRouter>
+        <Footer />
+      </div>
     );
   }
 }
